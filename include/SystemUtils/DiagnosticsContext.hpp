32c9d4d9dff047b32b528f671be48ed17b04aee0
Hatem Nabli <hatem.nabli@actemium.com>
2024-05-15 20:06:19 +0200

    Feat: this is an object which pushes a string onto the context stack of the diagnosticsSender message

---
+++ include/SystemUtils/DiagnosticsContext.hpp
@@ -0,0 +1,53 @@
+#ifndef SYSTEM_UTILS_DIAGNOSTICS_CONTEXT_HPP
+#define SYSTEM_UTILS_DIAGNOSTICS_CONTEXT_HPP
+
+/**
+ * @file DiagnosticsContext.hpp
+ * 
+ * This module declares the SystemAbstractions::DiagnosticsContext class.
+ * 
+ * © 2024 by Hatem Nabli
+*/
+
+#include "DiagnosticsSender.hpp"
+#include <memory>
+
+
+namespace SystemUtils {
+
+    /**
+     * This is a object which pushes a string onto the context
+     * stck of a diagnostics sender. The string pushec is popped when
+     * the object is destroyed.
+    */
+   class DiagnosticsContext {
+        // Lifecycle Management
+    public:
+        ~DiagnosticsContext() noexcept;
+        DiagnosticsContext(const DiagnosticsContext&) = delete;
+        DiagnosticsContext(DiagnosticsContext&&) noexcept = delete;
+        DiagnosticsContext& operator=(const DiagnosticsContext&) = delete;
+        DiagnosticsContext& operator=(DiagnosticsContext&&) noexcept = delete;
+    
+    public:
+
+        DiagnosticsContext(
+                DiagnosticsSender& DiagnosticsSender,
+                const std::string& context
+        ) noexcept;
+    private:
+        /**
+         * This is the type of structure that contains the private 
+         * properties of the instance. It is defined in the implementation
+         * and declared here to ensure that it is scoped inside the class.
+        */
+       struct  Impl;
+
+       /**
+        * This contains the private properties of the instance.
+       */
+       std::unique_ptr< Impl > impl_;
+   };
+}   
+
+#endif /* SYSTEM_UTILS_DIAGNOSTICS_CONTEXT_HPP */
---
+++ src/DiagnosticsContext.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file DiagnosticsContext.cpp
+ * 
+ * This module contain the implementation of the
+ * SystemUtils::DiagnosticsContext class.
+ * 
+ * © 2024 by Hatem Nabli
+*/
+
+#include <SystemUtils/DiagnosticsSender.hpp>
+#include <SystemUtils/DiagnosticsContext.hpp>
+
+namespace SystemUtils {
+
+    struct DiagnosticsContext::Impl
+    {
+        /**
+         * This is the sender upon which this class is pushing a context
+         * as long as the class instance exists.
+        */
+       DiagnosticsSender& diagnosticsSender;
+
+       /**
+        * 
+       */
+      Impl(DiagnosticsSender& newDiagnosticsSender) : 
+        diagnosticsSender(newDiagnosticsSender)
+        {
+            
+        } 
+    };
+
+    DiagnosticsContext::DiagnosticsContext(
+        DiagnosticsSender& diagnosticsSender,
+        const std::string& context
+    ) noexcept
+        : impl_(new Impl(diagnosticsSender))    
+    {
+        diagnosticsSender.PushContext(context);
+    }
+    
+    DiagnosticsContext::~DiagnosticsContext() noexcept {
+        impl_->diagnosticsSender.PopContext();
+    }
+}
